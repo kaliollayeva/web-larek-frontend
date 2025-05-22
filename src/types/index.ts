31a67da4982f28a59be3059fbbba7d187346e03d
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2025-05-22 21:53:05 +0500

    Документация 1

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+export interface ICard {
+    id: string;
+    title: string;
+    image: string;
+    description: string;
+    category: string;
+    price: number;
+}
+
+export interface IUser {
+    payment: string;
+    email: string;
+    phone: string; 
+    address: string;   
+    total: number;
+    items: string[];
+}
+
+// Данные карточки, используемые в каталоге
+export type ICardPublicInfo = Pick<ICard, 'category' | 'title' | 'image' | 'price' >;
+
+// Данные карточки, используемые в модальном окне
+export type ICardFullInfo = Pick<ICard, 'category' | 'title' | 'image' | 'price' | 'description'>;
+
+// Данные карточки, используемые в корзине
+export type ICardBasketInfo = Pick<ICard,  'title' | 'price' >;
+
+
